056639ff606468076824bb879e4e051cd85e6bc9
passyEche <echepaschallouianiefuna@gmail.com>
2022-01-11 20:23:26 +0100

    Initial commit

---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todos;
+
+class TodosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Todos::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+        return Todos::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'confirmed' => FALSE
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodosController;
+use App\Http\Controllers\AuthController;
+
+
+// use App\Models\Todos;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('/register ', [AuthController::class, 'register']);
+Route::post('/login ', [AuthController::class, 'login']);
+
+Route::get('/todos', [TodosController::class, 'index']);
+Route::resource('todos', TodosController::class);
+
+
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/todos', [TodosController::class, 'store']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+});
